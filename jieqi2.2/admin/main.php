d060625939a8dc1c714fe04a7cfd903827984859
kitgws <47107068+kitgws@users.noreply.github.com>
2019-01-29 22:18:39 +0800

    Add files via upload

---
+++ jieqi2.2/admin/main.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+<meta name="keywords" content="jieqi cms PHP 杰奇网站管理系统 书库 小说连载系统 小说程序开发 在线漫画系统 原创 文学" />
+<meta name="description" content="JIEQI CMS开发，基于PHP程序开发，含杰奇小说连载系统、杰奇漫画系统、杰奇vip电子书系统、杰奇论坛系统等，是原创文学、书库和小说网站的建站首选。" />
+<meta name="author" content="http://www.jieqi.com (jieqi cms)" />
+<meta name="copyright" content="Powered by JIEQI CMS &copy; 杰奇网络（http://www.jieqi.com）" />
+<meta name="generator" content="jieqi" />
+<link rel="stylesheet" type="text/css" media="all" href="/css/admin_right.css" />
+<script type="text/javascript" src="/js/common.js"></script>
+<script type="text/javascript" src="/js/admin.js"></script>
+
+</head>
+<body >
+<div id="content">
+<br />
+<br />
+<table width="100%"  border="0" cellspacing="2" cellpadding="3">
+  <tr>
+    <td>&nbsp;</td>
+    <td align="center">&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="center">您已成功登录，请点击左边链接使用相关功能！</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="center">&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
+<table class="grid" width="600" align="center">
+  <caption>模板使用申明</caption>
+  <tr>
+    <td class="head"><span class="hottext" style="font-size:14px;font-weight:bold;">当前版本：杰奇原创文学系统2.3</span></td>
+  </tr>
+  <tr>
+    <td class="odd">	
+<ul class="ulrow fsl" style="margin-left:50px;">
+<li>1.本模板只用于学习研究，禁止用于侵犯版权，违反法律的内容。</li>
+<li>2.本程序版权归杰奇网络所有，请支持官方正版程序。</li>
+<li>3.对于使用本程序出现的法律纠纷，“天吟模板店"概不负责。</li>
+<li>4.如果在模板使用出现问题，可以随时咨询本店铺客服。</li>
+<li>5.对于互联网其它卖家盗卖所得源码，本店不提供任何技术支持。</li>
+</ul>
+<a href="https://www.huzhan.com/ishop15400/" target="_blank" style="float:right;"><span class="hottext" style="font-size:14px;font-weight:bold;">&gt;&gt;前往本店</span></a>
+<br /><br />
+    </td>
+  </tr>
+  <tr>
+    <td class="foot">技术支持：<a href="http://www.jieqi.com" target="_blank">杰奇网络</a></td>
+  </tr>
+</table>
+
+</div>
+</body>
+</html>
---
+++ jieqi2.2/admin/vips.php
@@ -0,0 +1,186 @@
+<?php
+
+define("JIEQI_MODULE_NAME", "system");
+require_once ("../global.php");
+include_once (JIEQI_ROOT_PATH . "/class/power.php");
+$power_handler = &JieqiPowerHandler::getInstance("JieqiPowerHandler");
+$power_handler->getSavedVars("system");
+jieqi_checkpower($jieqiPower["system"]["adminconfig"], $jieqiUsersStatus, $jieqiUsersGroup, false, true);
+jieqi_loadlang("vips", JIEQI_MODULE_NAME);
+include_once (JIEQI_ROOT_PATH . "/lib/html/formloader.php");
+include_once (JIEQI_ROOT_PATH . "/class/vips.php");
+$vips_handler = &JieqivipsHandler::getInstance("JieqivipsHandler");
+
+if (empty($_REQUEST["action"])) {
+	$_REQUEST["action"] = "show";
+}
+
+switch ($_REQUEST["action"]) {
+case "new":
+	$errtext = "";
+
+	if (empty($_POST["caption"])) {
+		$errtext .= $jieqiLang["system"]["need_vip_caption"] . "<br />";
+	}
+
+	if (!is_numeric($_POST["minegold"])) {
+		$errtext .= $jieqiLang["system"]["need_minegold_num"] . "<br />";
+	}
+
+	if (!is_numeric($_POST["extraegold"])) {
+		$errtext .= $jieqiLang["system"]["need_extraegold_num"] . "<br />";
+	}
+
+	$_POST["minegold"] = intval($_POST["minegold"]);
+	$_POST["maxegold"] = intval($_POST["maxegold"]);
+	$_POST["viptype"] = intval($_POST["viptype"]);
+	$_POST["extraegold"] = intval($_POST["extraegold"]);
+	$_POST["extradiv"] = intval($_POST["extradiv"]);
+
+
+	if (empty($errtext)) {
+		$vips = $vips_handler->create();
+		$vips->setVar("caption", $_POST["caption"]);
+		$vips->setVar("minegold", $_POST["minegold"]);
+		$vips->setVar("maxegold", $_POST["maxegold"]);
+		$vips->setVar("viptype", $_POST["viptype"]);
+		$vips->setVar("extraegold", $_POST["extraegold"]);
+		$vips->setVar("extradiv", $_POST["extradiv"]);
+	//	$vips->setVar("viptype", "0");
+
+		if (!$vips_handler->insert($vips)) {
+			jieqi_printfail($jieqiLang["system"]["add_vip_failure"]);
+		}
+	}
+	else {
+		jieqi_printfail($errtext);
+	}
+
+	break;
+
+case "delete":
+	if (!empty($_REQUEST["id"])) {
+		$vips_handler->delete($_REQUEST["id"]);
+	}
+
+	break;
+
+case "update":
+	if (!empty($_REQUEST["id"]) && !empty($_POST["caption"])) {
+		$vips = $vips_handler->get($_REQUEST["id"]);
+
+		if (is_object($vips)) {
+			$errtext = "";
+
+			if (empty($_POST["caption"])) {
+				$errtext .= $jieqiLang["system"]["need_vip_caption"] . "<br />";
+			}
+             if (!is_numeric($_POST["minegold"])) {
+				$errtext .= $jieqiLang["system"]["need_minegold_num"] . "<br />";
+			}
+
+			if (!is_numeric($_POST["viptype"])) {
+				$errtext .= $jieqiLang["system"]["need_viptype_num"] . "<br />";
+			}
+
+			
+			if (!is_numeric($_POST["extraegold"])) {
+				$errtext .= $jieqiLang["system"]["need_extraegold_num"] . "<br />";
+			}
+			
+			if (!is_numeric($_POST["extradiv"])) {
+				$errtext .= $jieqiLang["system"]["need_extradiv_num"] . "<br />";
+			}
+
+			$_POST["minegold"] = intval($_POST["minegold"]);
+			$_POST["maxegold"] = intval($_POST["maxegold"]);
+			$_POST["viptype"] = intval($_POST["viptype"]);
+			$_POST["extraegold"] = intval($_POST["extraegold"]);
+			$_POST["extradiv"] = intval($_POST["extradiv"]);
+
+			if (empty($errtext)) {
+				$vips->setVar("caption", $_POST["caption"]);
+				$vips->setVar("minegold", $_POST["minegold"]);
+				$vips->setVar("maxegold", $_POST["maxegold"]);
+				$vips->setVar("viptype", $_POST["viptype"]);
+				$vips->setVar("extraegold", $_POST["extraegold"]);
+				$vips->setVar("extradiv", $_POST["extradiv"]);
+
+				if (!$vips_handler->insert($vips)) {
+					jieqi_printfail($jieqiLang["system"]["edit_vip_failure"]);
+				}
+			}
+			else {
+				jieqi_printfail($errtext);
+			}
+		}
+	}
+
+	break;
+
+case "edit":
+	if (!empty($_REQUEST["id"])) {
+		$vips = $vips_handler->get($_REQUEST["id"]);
+
+		if (is_object($vips)) {
+			include_once (JIEQI_ROOT_PATH . "/admin/header.php");
+			$vips_form = new JieqiThemeForm($jieqiLang["system"]["edit_vip"], "vipsedit", JIEQI_URL . "/admin/vips.php");
+			$vips_form->addElement(new JieqiFormText($jieqiLang["system"]["table_vips_caption"], "caption", 30, 250, $vips->getVar("caption", "e")), true);
+			$vips_form->addElement(new JieqiFormTextArea($jieqiLang["system"]["table_vips_minegold"], "minegold", $vips->getVar("minegold", "e"), 5, 50));
+			$vips_form->addElement(new JieqiFormTextArea($jieqiLang["system"]["table_vips_maxegold"], "maxegold", $vips->getVar("minegold", "e"), 5, 50));
+			$vips_form->addElement(new JieqiFormTextArea($jieqiLang["system"]["table_vips_viptype"], "viptype", $vips->getVar("viptype", "e"), 5, 50));
+			$vips_form->addElement(new JieqiFormTextArea($jieqiLang["system"]["table_vips_extraegold"], "extraegold", $vips->getVar("extraegold", "e"), 5, 50));
+			$vips_form->addElement(new JieqiFormTextArea($jieqiLang["system"]["table_vips_extradiv"], "extradiv", $vips->getVar("extradiv", "e"), 5, 50));
+			$vips_form->addElement(new JieqiFormHidden("action", "update"));
+			$vips_form->addElement(new JieqiFormHidden("id", $_REQUEST["id"]));
+			$vips_form->addElement(new JieqiFormButton("&nbsp;", "submit", LANG_SAVE, "submit"));
+			$jieqiTpl->assign("jieqi_contents", "<br />" . $vips_form->render(JIEQI_FORM_MAX) . "<br />");
+			include_once (JIEQI_ROOT_PATH . "/admin/footer.php");
+			exit();
+		}
+	}
+
+	break;
+}
+
+include_once (JIEQI_ROOT_PATH . "/admin/header.php");
+$criteria = new CriteriaCompo();
+$criteria->setSort("minegold");
+$criteria->setOrder("ASC");
+$vips_handler->queryObjects($criteria);
+$vips = array();
+$vipary = array();
+$i = 0;
+
+while ($v = $vips_handler->getObject()) {
+	$nameary = explode(" ", $v->getVar("caption"));
+	$vipary[$v->getVar("vipid")] = array("caption" => $nameary[0], "minegold" => $v->getVar("minegold"),"maxegold" => $v->getVar("maxegold"), "extraegold" => $v->getVar("extraegold"), "extradiv" => $v->getVar("extradiv"),"viptype" => $v->getVar("viptype"));
+	$vips[$i]["vipid"] = $v->getVar("vipid");
+	$vips[$i]["caption"] = implode("<br />", $nameary);
+	$vips[$i]["minegold"] = $v->getVar("minegold");
+	$vips[$i]["maxegold"] = $v->getVar("maxegold");
+	$vips[$i]["extraegold"] = $v->getVar("extraegold");
+	$vips[$i]["extradiv"] = $v->getVar("extradiv");
+	$vips[$i]["viptype"] = $v->getVar("viptype");
+	$i++;
+	if ((!empty($_REQUEST["id"]) || !empty($_POST["caption"])) && (0 < count($vipary))) {
+	jieqi_setconfigs("vips", "jieqiVips", $vipary, "system");
+}
+}
+
+$jieqiTpl->assign_by_ref("vips", $vips);
+$vips_form = new JieqiThemeForm($jieqiLang["system"]["add_vip"], "vipsnew", JIEQI_URL . "/admin/vips.php");
+$vips_form->addElement(new JieqiFormText($jieqiLang["system"]["table_vips_caption"], "caption", 30, 250, ""), true);
+$vips_form->addElement(new JieqiFormText($jieqiLang["system"]["table_vips_minegold"], "minegold", 30, 50, ""), true);
+$vips_form->addElement(new JieqiFormText($jieqiLang["system"]["table_vips_maxegold"], "maxegold", 30, 50, ""), true);
+$vips_form->addElement(new JieqiFormText($jieqiLang["system"]["table_vips_viptype"], "viptype", 30, 50, ""), true);
+$vips_form->addElement(new JieqiFormText($jieqiLang["system"]["table_vips_extraegold"], "extraegold", 30, 50, ""), true);
+$vips_form->addElement(new JieqiFormText($jieqiLang["system"]["table_vips_extradiv"], "extradiv", 30, 50, ""), true);
+$vips_form->addElement(new JieqiFormHidden("action", "new"));
+$vips_form->addElement(new JieqiFormButton("&nbsp;", "submit", $jieqiLang["system"]["add_vip"], "submit"));
+$jieqiTpl->assign("form_addvip", "<br />" . $vips_form->render(JIEQI_FORM_MAX) . "<br />");
+$jieqiTpl->setCaching(0);
+$jieqiTset["jieqi_contents_template"] = JIEQI_ROOT_PATH . "/templates/admin/vips.html";
+include_once (JIEQI_ROOT_PATH . "/admin/footer.php");
+
+?>
